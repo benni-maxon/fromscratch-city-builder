ff3ce21e4bda7d36d347e05aa6f67b632e044b90
benni maxon <benni.maxon@gmail.com>
2022-11-04 10:51:54 -0700

    completed slice 4 and added default imgs and favorite thing

--- app.js
+++ app.js
@@ -2,17 +2,24 @@
 const farmSelect = document.getElementById('farm-select');
 const farmImg = document.getElementById('farm-img');
 const farmChangeCounter = document.getElementById('farm-change-counter');
+
 const professionSelect = document.getElementById('profession-select');
 const professionImg = document.getElementById('profession-img');
 const professionChangeCounter = document.getElementById('profession-change-counter');
+
 const romanceSelect = document.getElementById('romance-select');
 const romanceImg = document.getElementById('romance-img');
 const romanceChangeCounter = document.getElementById('romance-change-counter');
+
+const favoriteInput = document.getElementById('favorite-input');
+const okButton = document.getElementById('ok-button');
+const favoriteList = document.getElementById('favorite-list');
 
 /* State */
 let farmCounter = 0;
 let professionCounter = 0;
 let romanceCounter = 0;
+let favoriteArray = [];
 
 /* Events */
 farmSelect.addEventListener('change', () => {
@@ -46,6 +53,26 @@ romanceSelect.addEventListener('change', () => {
     romanceCounter++;
 
     romanceChangeCounter.textContent = `You've changed partners ${romanceCounter} times`;
+});
+
+okButton.addEventListener('click', () => {
+    const newFavorite = favoriteInput.value;
+
+    favoriteArray.push(newFavorite);
+
+    favoriteList.textContent = '';
+
+    for (let favorite of favoriteArray) {
+        const p = document.createElement('p');
+
+        p.classList.add('favorite');
+
+        p.textContent = favorite;
+
+        favoriteList.append(p);
+    }
+
+    favoriteInput.value = '';
 });
 /* Display Functions */
 
