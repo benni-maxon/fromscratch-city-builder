5e6a6198b1e2f4b8d2b5acfe3ac1581984e71308
benni maxon <benni.maxon@gmail.com>
2022-11-04 10:07:07 -0700

    added a fully functional slice 2

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const farmSelect = document.getElementById('farm-select');
 const farmImg = document.getElementById('farm-img');
 const farmChangeCounter = document.getElementById('farm-change-counter');
+const professionSelect = document.getElementById('profession-select');
+const professionImg = document.getElementById('profession-img');
+const professionChangeCounter = document.getElementById('profession-change-counter');
 
 /* State */
 let farmCounter = 0;
+let professionCounter = 0;
 
 /* Events */
 farmSelect.addEventListener('change', () => {
@@ -18,6 +22,16 @@ farmSelect.addEventListener('change', () => {
     farmChangeCounter.textContent = `You've changed farms ${farmCounter} times`;
 });
 
+professionSelect.addEventListener('change', () => {
+    const id = professionSelect.value;
+
+    professionImg.src = `./assets/profession-${id}.gif`;
+    professionImg.alt = `${id} profession thumbnail`;
+
+    professionCounter++;
+
+    professionChangeCounter.textContent = `You've changed professions ${professionCounter} times`;
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
