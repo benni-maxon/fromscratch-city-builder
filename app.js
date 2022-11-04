52c66fa33fa49e2b09c780dfec9e741c4e6e9caa
Monika <moncobra@comcast.net>
2022-11-04 10:21:13 -0700

    Slice 3 finished, needs img tag

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const farmChangeCounter = document.getElementById('farm-change-counter');
 const professionSelect = document.getElementById('profession-select');
 const professionImg = document.getElementById('profession-img');
 const professionChangeCounter = document.getElementById('profession-change-counter');
+const romanceSelect = document.getElementById('romance-select');
+const romanceImg = document.getElementById('romance-img');
+const romanceChangeCounter = document.getElementById('romance-change-counter');
 
 /* State */
 let farmCounter = 0;
 let professionCounter = 0;
+let romanceCounter = 0;
 
 /* Events */
 farmSelect.addEventListener('change', () => {
@@ -26,11 +30,22 @@ professionSelect.addEventListener('change', () => {
     const id = professionSelect.value;
 
     professionImg.src = `./assets/profession-${id}.gif`;
-    professionImg.alt = `${id} profession thumbnail`;
+    professionImg.alt = `${id} profession gif`;
 
     professionCounter++;
 
     professionChangeCounter.textContent = `You've changed professions ${professionCounter} times`;
+});
+
+romanceSelect.addEventListener('change', () => {
+    const id = romanceSelect.value;
+
+    romanceImg.src = `./assets/romance-${id}.png`;
+    romanceImg.alt = `${id} character portrait`;
+
+    romanceCounter++;
+
+    romanceChangeCounter.textContent = `You've changed partners ${romanceCounter} times`;
 });
 /* Display Functions */
 
